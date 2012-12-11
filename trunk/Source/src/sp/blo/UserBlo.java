d62d53945eb6395f66e2db12eadb3cf64ceed22a
hailam349@gmail.com <hailam349@gmail.com@4742654a-b9f5-f514-53d8-c622825f6766>
2012-12-11 16:22:37 +0000

    add folder src

---
+++ trunk/Source/src/sp/blo/UserBlo.java
@@ -0,0 +1,37 @@
+package sp.blo;
+
+import sp.dao.UserDao;
+import sp.dto.User;
+
+public class UserBlo {
+	private static UserDao userDao = new UserDao();
+
+	/**
+	 * checkUser
+	 * @param id
+	 * @return int
+	 */
+	public static int checkUser(String id) {
+		User user = new User();
+		if (userDao.checkExistUser(id)) {
+			user = userDao.getUser(id);
+			if (user != null) {
+				if (user.getIdUser().equals(user.getPassword())) {
+					// success
+					return 2;
+				} else {
+					// password bi sai
+					return 1;
+				}
+			}
+		}
+		// user ko ton tai
+		return 0;
+	}
+	
+	public static int checkRole(String value){
+		return 1;
+	}
+	
+	
+}
