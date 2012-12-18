6bbb1a384c3b02d4f03076a8b3f281943574c54d
hailam349@gmail.com <hailam349@gmail.com@4742654a-b9f5-f514-53d8-c622825f6766>
2012-12-18 08:45:20 +0000

    add Report

--- trunk/Source/src/sp/util/CommonUtil.java
+++ trunk/Source/src/sp/util/CommonUtil.java
@@ -1,7 +1,29 @@
 package sp.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommonUtil {
 	
-	
+	/*
+	 * tinh so page dua tren so record
+	 */
+	public static List<String> countOrganizationAll(int count) {
+		
+		List<String> pageList = new ArrayList<String>();
+		int page = 0;
+		if (count > 0) {
+			page = count / Constant.RECORD;
+			if (count % Constant.RECORD > 0) {
+				page = page + 1;
+			}
+			page = page + 1;
+			for (int i = 1; i < page; i++) {
+				pageList.add(String.valueOf(i));
+			}
+		}
+		return pageList;
+
+	}
 
 }
