731e9243372af4882cb6113de12b9eccf694cfa0
hailam349 <hailam349@gmail.com>
2012-12-18 08:45:20 +0000

    add Report

--- Source/src/sp/util/Constant.java
+++ Source/src/sp/util/Constant.java
@@ -3,9 +3,29 @@ package sp.util;
 public class Constant {
 	public final static String SUCCESS ="success";
 	public final static int RECORD =10;
+	
+	//ORGANIZATION
 	public final static String ORGANIZATION_LIST ="org_list";
 	public final static String ORG_PAGE_LIST ="org_page_list";
 	public final static String ORGANIZATION ="organization";
 	public final static String ORG_PAGE_NUMBER ="org_page";
 	public final static String ORG_TOTAL_NUMBER ="org_total_number";
+	
+	//REPORT
+	//DEFINE REPORT_STATUS
+	public final static String NEW = "new"; //1
+	public final static String REVIEW = "review"; //2
+	public final static String REQUEST_UPDATE = "request_update"; //3
+	public final static String UPDATED = "updated"; //4
+	public final static String FINISH = "finish";   //5
+	public final static String CANCEL = "cancel";   //6
+	public final static String CLOSE = "close";     //0
+	
+	//SESSION
+	public final static String REPORT_LIST ="report_list";
+	public final static String REPORT ="report";
+	public final static String REPORT_PAGE_LIST ="report_page_list";
+	public final static String REPORT_PAGE_NUMBER ="report_page_number";
+	public final static String REPORT_TOTAL_NUMBER ="report_total_number";
+	
 }
