39725e3dbd0ffedc5365ea9396dc1f7f57a38c17
hailam349@gmail.com <hailam349@gmail.com@4742654a-b9f5-f514-53d8-c622825f6766>
2013-01-02 15:06:06 +0000

    add js for report

--- trunk/Source/war/javascripts/quanlyyeucauscripts.js
+++ trunk/Source/war/javascripts/quanlyyeucauscripts.js
@@ -315,3 +315,237 @@ function checkStatusLoginByGoogleAccount()
 }
 
 // ------------- Các hàm xử lý trong trang login.jsp
+
+
+function getListReport(flag,page)
+{
+	alert(flag);
+	var form = document.getElementById("sortForm");
+	
+	var select;
+	
+	if (flag == 1){
+		
+		select = form.elements["project"];
+	}
+	else if (flag == 2){
+		select = form.elements["req"];
+	}
+	else if (flag == 3){
+		select = form.elements["group"];
+	}
+	else if (flag == 4){
+		select = form.elements["task"];
+	}
+	else if (flag == 5){
+		select = form.elements["status"];
+	}
+	
+	var sort_value =select[select.selectedIndex].value;
+	
+	
+	ajax_getListReport(page,select.name,sort_value);
+	
+	
+	ajax_getCountAndCountPage(page);
+}
+
+function getListReportByPage(page)
+{
+//	var form = document.getElementById("sortForm");
+//	
+//	var select_page = form.elements["select_page"];
+	ajax_getListReportByPage(page)
+}
+
+//tinh so page
+function ajax_getCountAndCountPage(page, flag)
+{
+  var xhr = createXHR();
+  xhr.onreadystatechange = function(){
+      if (xhr.readyState == 4){
+          if ((xhr.status  >= 200  &&  xhr.status  <  300) || xhr.status == 304){
+
+              var pageInfo = JSON.parse(xhr.responseText);      
+              draw_phantrang(pageInfo,page);
+              
+
+          } else {
+              alert("Request was unsuccessful: " + xhr.status);
+          }
+      }
+  };
+
+  var url = "countPageList.do";
+  
+  //my report
+  if (flag == 1){
+  	 url = addURLParam(url, "flag", "1");
+  }
+  xhr.open("get", url, true);
+  xhr.send(null);
+}
+
+
+//get danh sach report
+function ajax_getListReport(page, sort_name, sort_value)
+{
+	
+  var xhr = createXHR();
+  xhr.onreadystatechange = function(){
+      if (xhr.readyState == 4){
+          if ((xhr.status  >= 200  &&  xhr.status  <  300) || xhr.status == 304){
+
+              var list_report = JSON.parse(xhr.responseText);
+              //draw_phantrang(list_Account[0].SOLUONG,page);
+
+              draw_table_danhsach_report(list_report, list_report.length,0);
+
+              //undraw_loading();
+          } else {
+              alert("Request was unsuccessful: " + xhr.status);
+          }
+      }
+  };
+
+  
+  var url = "dislayReportListPaging.do";
+  url = addURLParam(url, "KEY", "LIST_REPORT");
+  url = addURLParam(url, sort_name, sort_value);
+  url = addURLParam(url, "page", page);
+  url = addURLParam(url, "ajax", "1");
+//  url = addURLParam(url, select_group.name, select_group[select_group.selectedIndex].value);
+  
+  
+  xhr.open("get", url, true);
+  xhr.send(null);
+}
+
+//get danh sach report by page
+function ajax_getListReportByPage(page)
+{
+	
+//  var form = document.getElementById("sortForm");
+//  var select_group = form.elements["group"];
+  
+  var xhr = createXHR();
+  xhr.onreadystatechange = function(){
+      if (xhr.readyState == 4){
+          if ((xhr.status  >= 200  &&  xhr.status  <  300) || xhr.status == 304){
+
+              var list_report = JSON.parse(xhr.responseText);
+              //draw_phantrang(list_Account[0].SOLUONG,page);
+
+              draw_table_danhsach_report(list_report, list_report.length,0);
+
+              //undraw_loading();
+          } else {
+              alert("Request was unsuccessful: " + xhr.status);
+          }
+      }
+  };
+
+  
+  var url = "dislayReportListPaging.do";
+  url = addURLParam(url, "KEY", "LIST_REPORT");
+  url = addURLParam(url, "page", page);
+  url = addURLParam(url, "ajax", "1");
+//  url = addURLParam(url, select_group.name, select_group[select_group.selectedIndex].value);
+  
+  
+  xhr.open("get", url, true);
+  xhr.send(null);
+}
+
+//--- ve
+function draw_table_danhsach_report(list_report,length,index)
+{
+	// name cá»§a input checkbox
+  var tr, td, i;
+  var table = document.getElementById("table_report_list");
+
+  // tháº» body cÅ©
+  var body = table. tBodies[0];
+
+  // váº½ má»™t thá»§ body má»›i chá»©a dá»¯ liá»‡u
+  var tbody = document.createElement("tbody");
+  table.replaceChild(tbody,body);
+  for (i = index; i<length; i++)
+  {
+  	
+      tr = tbody.insertRow(tbody.rows.length);
+      tr.setAttribute("align", "center");
+
+      td = tr.insertCell(tr.cells.length);
+      var input = document.createElement("input");
+      input.setAttribute("type","checkbox");
+      input.setAttribute("name","check");
+      input.setAttribute("value",list_report[i].id);
+      td.appendChild(input);
+     
+      
+
+      td = tr.insertCell(tr.cells.length);
+      var a =  document.createElement("a");
+      a.setAttribute("href","javascript: void(0)");
+      a.setAttribute("onclick","ajax_getAccount('" +list_report[i].id+"');");
+      a.innerHTML = list_report[i].id;
+      td.appendChild(a);
+             
+      
+      td = tr.insertCell(tr.cells.length);
+      td.innerHTML = list_report[i].title;
+      
+      td = tr.insertCell(tr.cells.length);
+      td.innerHTML = list_report[i].idUser;
+      
+      td = tr.insertCell(tr.cells.length);
+      var a =  document.createElement("a");
+      a.setAttribute("href","/downloadFile.do?id="+list_report[i].fileId);
+//      a.setAttribute("onclick","ajax_getAccount('" +list_report[i].id+"');");
+      a.innerHTML = "Download";
+      td.appendChild(a);
+      
+  }
+  
+}
+//-- Change Mode --
+
+function changeMode()
+{
+  
+  var xhr = createXHR();
+  xhr.onreadystatechange = function(){
+      if (xhr.readyState == 4){
+          if ((xhr.status  >= 200  &&  xhr.status  <  300) || xhr.status == 304){
+          	
+          } else {
+              alert("Request was unsuccessful: " + xhr.status);
+          }
+      }
+  };
+
+  
+  var url = "getlistaccount.do";
+  url = addURLParam(url, sort_id, sort_value);
+  url = addURLParam(url, "page", page);
+//  url = addURLParam(url, select_group.name, select_group[select_group.selectedIndex].value);
+  
+  
+  xhr.open("get", url, true);
+  xhr.send(null);
+}
+function checkSubmit()
+{
+	var file = document.getElementById("fileId").value;
+	if (file.length > 0)
+		{
+		return true;
+		}
+	else
+		{
+		alert('Vui lòng chọn file');
+		return false;
+		}
+	
+}
