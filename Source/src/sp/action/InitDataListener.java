14df7dc0892e20e783cbc246d5c48acf5a658eb7
hailam349 <hailam349@gmail.com>
2013-01-17 18:15:36 +0000

    init data when start up app

---
+++ Source/src/sp/action/InitDataListener.java
@@ -0,0 +1,23 @@
+package sp.action;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import sp.blo.CommonBlo;
+/**
+ * @author ThuyNT
+ */
+public class InitDataListener implements ServletContextListener {
+    /** . */
+    private ServletContext servletContext;
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+    	CommonBlo.createData();
+
+    }
+
+}
